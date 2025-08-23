e07249bb666f15a82295ebc79d8a7c6189629d54
Lê Đăng Khoa <khoald.24it@vku.udn.vn>
2025-08-23 12:21:52 +0700

    Lần 5

--- src/Components/B_Trangchu.jsx
+++ src/Components/B_Trangchu.jsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import TrangTri1 from "../assets/img/TrangChu_TrangTri_1.png"
 import TrangTri2 from "../assets/img/TrangChu_TrangTri_2.png"
 import TrangTri4 from "../assets/img/TrangChu_TrangTri_4.png"
 import '../Design/Trangchu.scss';
 
 function Trangchu() {
+  const navigate = useNavigate();
+
     return <>
     {/* Trang 1 */}
     <div className="Trang_1">
@@ -24,9 +27,14 @@ function Trangchu() {
         d="M0,265 C240,335 480,345 720,315
          C960,290 1200,270 1440,282"/>
     </svg>
+      <button className="Trang1_button" onClick={() => navigate("/gioithieu")}>Tìm Hiểu Thêm</button>
       <p className="Trang1_chuchung Trang1_chu1 Trang1_chuchungchung">QUỸ HỖ TRỢ</p>
       <p className="Trang1_chu2 Trang1_chuchungchung">TRẺ EM</p>
       <p className="Trang1_chuchung Trang1_chu3 Trang1_chuchungchung">VÙNG SÂU VÙNG XA</p>
+      <p className="Trang1_chu4">Vùng sâu vùng xa còn nhiều thiếu thốn, đường đi khó khăn, <br /> 
+      đời sống vất vả. Nhưng nơi ấy sáng lên nghị lực, tình người <br /> 
+      chân chất và khát vọng vươn lên, nhắc nhở chúng ta chung <br />
+      tay góp sức để san sẻ yêu thương, xây dựng tương lai.</p>
     </div>
 
     {/* Trang 2 */}
